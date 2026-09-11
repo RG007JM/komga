7b01f1d09f7c4bd1d667b293ee2c1551f1456667
RG007JM <vamacoba@tuta.io>
2026-09-11 14:17:59 +0200

    style(kobo): format kepub size cache repository

--- komga/src/main/kotlin/org/gotson/komga/interfaces/api/kobo/KoboKepubSizeCacheRepository.kt
+++ komga/src/main/kotlin/org/gotson/komga/interfaces/api/kobo/KoboKepubSizeCacheRepository.kt
@@ -14,26 +14,27 @@ class KoboKepubSizeCacheRepository(
 
   fun findByBookId(bookId: String): Entry? {
     dataSource.connection.use { connection ->
-      connection.prepareStatement(
-        """
-        SELECT SOURCE_FILE_HASH, KEPUB_FILE_SIZE
-        FROM KOBO_KEPUB_SIZE_CACHE
-        WHERE BOOK_ID = ?
-        """.trimIndent(),
-      ).use { statement ->
-        statement.setString(1, bookId)
+      connection
+        .prepareStatement(
+          """
+          SELECT SOURCE_FILE_HASH, KEPUB_FILE_SIZE
+          FROM KOBO_KEPUB_SIZE_CACHE
+          WHERE BOOK_ID = ?
+          """.trimIndent(),
+        ).use { statement ->
+          statement.setString(1, bookId)
 
-        statement.executeQuery().use { resultSet ->
-          return if (resultSet.next()) {
-            Entry(
-              sourceFileHash = resultSet.getString("SOURCE_FILE_HASH"),
-              kepubFileSize = resultSet.getLong("KEPUB_FILE_SIZE"),
-            )
-          } else {
-            null
+          statement.executeQuery().use { resultSet ->
+            return if (resultSet.next()) {
+              Entry(
+                sourceFileHash = resultSet.getString("SOURCE_FILE_HASH"),
+                kepubFileSize = resultSet.getLong("KEPUB_FILE_SIZE"),
+              )
+            } else {
+              null
+            }
           }
         }
-      }
     }
   }
 
@@ -43,24 +44,25 @@ class KoboKepubSizeCacheRepository(
     kepubFileSize: Long,
   ) {
     dataSource.connection.use { connection ->
-      connection.prepareStatement(
-        """
-        INSERT INTO KOBO_KEPUB_SIZE_CACHE (
-          BOOK_ID,
-          SOURCE_FILE_HASH,
-          KEPUB_FILE_SIZE
-        )
-        VALUES (?, ?, ?)
-        ON CONFLICT(BOOK_ID) DO UPDATE SET
-          SOURCE_FILE_HASH = excluded.SOURCE_FILE_HASH,
-          KEPUB_FILE_SIZE = excluded.KEPUB_FILE_SIZE
-        """.trimIndent(),
-      ).use { statement ->
-        statement.setString(1, bookId)
-        statement.setString(2, sourceFileHash)
-        statement.setLong(3, kepubFileSize)
-        statement.executeUpdate()
-      }
+      connection
+        .prepareStatement(
+          """
+          INSERT INTO KOBO_KEPUB_SIZE_CACHE (
+            BOOK_ID,
+            SOURCE_FILE_HASH,
+            KEPUB_FILE_SIZE
+          )
+          VALUES (?, ?, ?)
+          ON CONFLICT(BOOK_ID) DO UPDATE SET
+            SOURCE_FILE_HASH = excluded.SOURCE_FILE_HASH,
+            KEPUB_FILE_SIZE = excluded.KEPUB_FILE_SIZE
+          """.trimIndent(),
+        ).use { statement ->
+          statement.setString(1, bookId)
+          statement.setString(2, sourceFileHash)
+          statement.setLong(3, kepubFileSize)
+          statement.executeUpdate()
+        }
     }
   }
 }

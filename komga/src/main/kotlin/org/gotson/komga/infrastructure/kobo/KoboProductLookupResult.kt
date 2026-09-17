7a6f9588e5d0500a31ea7f7987bc8467632b312e
RG007JM <vamacoba@tuta.io>
2026-09-18 00:36:43 +0200

    feat(kobo): extract series id during product lookup

--- komga/src/main/kotlin/org/gotson/komga/infrastructure/kobo/KoboProductLookupResult.kt
+++ komga/src/main/kotlin/org/gotson/komga/infrastructure/kobo/KoboProductLookupResult.kt
@@ -3,6 +3,7 @@ package org.gotson.komga.infrastructure.kobo
 sealed interface KoboProductLookupResult {
   data class Found(
     val productId: String,
+    val seriesId: String? = null,
   ) : KoboProductLookupResult
 
   data object NotFound : KoboProductLookupResult
